9b9dd35869c04d28e2a840771b11bf9094364226
Triskail <tomli@utexas.edu>
2019-05-17 17:49:08 -0500

    hero sort now auto adjusts to stay under character limit

--- xanderbot/command/hero_sort.py
+++ xanderbot/command/hero_sort.py
@@ -11,7 +11,7 @@ from feh.unitlib import UnitLib
 
 FROM_SYNONYMS = re.compile(r'(?:\s+|^)(?:in|from|within|among(?:st)?)\s+')
 
-PAGE_LIMIT = 20
+PAGE_LIMIT = 24
 
 
 class HeroSort(CmdDefault):
@@ -25,7 +25,8 @@ class HeroSort(CmdDefault):
     class Data(object):
 
         __slots__ = (
-            'embed', 'results', 'search_param', 'sort_param', 'page_start'
+            'embed', 'results', 'search_param', 'sort_param', 'page_start',
+            'page_limit'
         )
 
         embed: Embed
@@ -33,16 +34,22 @@ class HeroSort(CmdDefault):
         search_param: str
         sort_param: str
         page_start: int
+        page_limit: int
 
     @staticmethod
-    def format_sort(embed, results, search_param, sort_param, start=0):
+    def format_sort(embed, results, search_param, sort_param, start=0,
+                    page_limit=PAGE_LIMIT):
         if not results:
             start = -1
             end = 0
             result = 'No results!'
         else:
-            end = min(start + PAGE_LIMIT, len(results))
-            result = '\n'.join(results[start:end])
+            while True:
+                end = min(start + page_limit, len(results))
+                result = '\n'.join(results[start:end])
+                if len(result) < 2000:
+                    break
+                page_limit -= 4
         if search_param:
             matching = f' matching "{search_param}"'
         else:
@@ -51,7 +58,7 @@ class HeroSort(CmdDefault):
             f'Heroes{matching} sorted by ({sort_param or "None"}) '
             f'({start + 1} - {end} of {len(results)}):')
         embed.description = result
-        return embed
+        return embed, page_limit
 
     @staticmethod
     async def cmd(params, user_id):
@@ -78,8 +85,8 @@ class HeroSort(CmdDefault):
                     emojis=HeroSort.REACT_MENU, callback=HeroSort.react),
             )
         embed = Embed()
-        HeroSort.format_sort(
-            embed, sorted_list, search_terms, sort_terms, 0)
+        embed, page_limit = HeroSort.format_sort(
+            embed, sorted_list, search_terms, sort_terms, 0, PAGE_LIMIT)
         if any(bad_args):
             content = ('I did not understand the following: '
                        f'{", ".join(bad_args)}')
@@ -89,7 +96,7 @@ class HeroSort(CmdDefault):
         react_menu = ReactMenu(
             emojis=HeroSort.REACT_MENU,
             data=HeroSort.Data(
-                embed, sorted_list, search_terms, sort_terms, 0),
+                embed, sorted_list, search_terms, sort_terms, 0, page_limit),
             callback=HeroSort.react,
         )
         return ReplyPayload(content=content, embed=embed, reactable=react_menu)
@@ -113,6 +120,7 @@ class HeroSort(CmdDefault):
                 return ReactEditPayload(delete=True)
         else:
             return ReactEditPayload()
-        HeroSort.format_sort(data.embed, data.results, data.search_param,
-                             data.sort_param, data.page_start)
+        data.embed, data.page_limit = HeroSort.format_sort(
+            data.embed, data.results, data.search_param, data.sort_param,
+            data.page_start, data.page_limit)
         return ReactEditPayload(embed=data.embed, delete=True)
